391e1c51b567f6c6e8bc3a4912f56519bfadd432
DritaSkeraj <37184676+DritaSkeraj@users.noreply.github.com>
2021-01-23 21:10:51 +0100

    adds cors

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const cors = require("cors")
 const listEndpoints = require("express-list-endpoints")
 const examsRouter = require("./services/exams")
 const {
@@ -9,9 +10,26 @@ const {
 
 const server = express();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3030
 
 server.use(express.json());
+
+const whiteList = 'http://127.0.0.1:3000/exams'
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whiteList.indexOf(origin) !== -1) {
+      // allowed
+      callback(null, true)
+    } else {
+      // Not allowed
+      callback(new Error("NOT ALLOWED - CORS ISSUES"))
+    }
+  },
+}
+server.use(cors()) // CROSS ORIGIN RESOURCE SHARING
+
+
 server.use("/exams", examsRouter);
 
 server.use(badRequestHandler)
